d45bcfc37cad19737d6af29cdf26d5aaedd48ef6
Laurentiu Turcu <turculaurentiu91@gmail.com>
2019-06-23 00:43:59 +0200

    Added confirmation dialog when deleting request

--- resources/js/Pages/StandardRequestsIndex.vue
+++ resources/js/Pages/StandardRequestsIndex.vue
@@ -4,16 +4,16 @@
 
     <h3 v-if="$page.requests.data.length <= 0">Nessuna Richiesta Trovata</h3>
     <div class="w3-bar w3-margin" v-if="$page.requests.data.length > 0">
-      <inertia-link 
-        v-for="n in $page.requests.last_page" 
-        :key="n" 
+      <inertia-link
+        v-for="n in $page.requests.last_page"
+        :key="n"
         :href="`/express-requests?page=${n}`"
         class="w3-button"
         :class="{'w3-green' : $page.requests.current_page === n}"
       >{{n}}</inertia-link>
     </div>
 
-    <table 
+    <table
       v-if="$page.requests.data.length > 0"
       class="w3-table-all w3-hoverable w3-margin-top"
     >
@@ -30,8 +30,8 @@
         </tr>
       </thead>
       <tbody>
-        <tr 
-          v-for="req in $page.requests.data" 
+        <tr
+          v-for="req in $page.requests.data"
           v-bind:key="req.id"
           class="pointer-cursor"
         >
@@ -50,28 +50,38 @@
             </inertia-link>
           </td>
           <td>
-            <inertia-link
-              :href="`/standard-requests/${req.id}`"
+            <button
               class="w3-button w3-text-red"
               method="delete"
+              @click.prevent="() => setupRequestForDeletion(req)"
             >
               <i class="fa fa-times w3-xlarge"></i>
-            </inertia-link>
+            </button>
           </td>
         </tr>
       </tbody>
     </table>
 
     <div class="w3-bar w3-margin" v-if="$page.requests.data.length > 0">
-      <inertia-link 
-        v-for="n in $page.requests.last_page" 
-        :key="n" 
+      <inertia-link
+        v-for="n in $page.requests.last_page"
+        :key="n"
         :href="`/express-requests?page=${n}`"
         class="w3-button"
         :class="{'w3-green' : $page.requests.current_page === n}"
       >{{n}}</inertia-link>
     </div>
 
+
+    <ConfirmationDialog
+        title="Cancellare Richiesta"
+        v-if="requestPendingDeletion"
+        v-on:confirm="handleConfirmationSubmit"
+    >
+        Sei sicuro di voler eliminare la richiesta da
+        {{requestPendingDeletion.company_name}}? <br>
+        Questa azione è irreversibile.
+    </ConfirmationDialog>
   </Layout>
 </template>
 
@@ -87,5 +97,22 @@ import Layout from '@/Shared/Layout';
 
 export default {
   components: {Layout},
+  data: function() {
+      return {
+          requestPendingDeletion: null,
+      };
+  },
+  methods: {
+    setupRequestForDeletion: function(req) {
+        this.requestPendingDeletion = { ...req };
+    },
+    handleConfirmationSubmit: function(confirmed) {
+        if (confirmed) {
+            this.$inertia
+            .delete(`/standard-requests/${this.requestPendingDeletion.id}`);
+        }
+        this.requestPendingDeletion = null;
+    }
+  }
 }
 </script>
