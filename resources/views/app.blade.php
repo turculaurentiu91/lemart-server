253999470522d048b0d98f3f5b4e94ffdfda5292
Laurentiu Turcu <turculaurentiu91@gmail.com>
2019-06-22 23:45:39 +0200

    Added favicon

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -7,6 +7,9 @@
     <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
     <script src="{{ mix('/js/app.js') }}" defer></script>
     <script src="https://use.fontawesome.com/8cbdc88575.js"></script>
+
+    <link rel="shortcut icon" href="/favicon.ico" type="image/x-icon">
+    <link rel="icon" href="/favicon.ico" type="image/x-icon">
 </head>
 <body>
 
