e6bf69a8e96cf23ef1c97325ce6d1df7bb2771e9
Laurentiu Turcu <turculaurentiu91@gmail.com>
2019-06-23 07:39:10 +0200

    Added flash messages

--- resources/js/Shared/Layout.vue
+++ resources/js/Shared/Layout.vue
@@ -1,13 +1,13 @@
 <template>
   <div class="w3-white page">
-    <nav 
+    <nav
       class="w3-sidebar w3-bar-block sidebar w3-green w3-border-right"
-      v-bind:class="{'sidebar--closed': isNavClosed}"  
+      v-bind:class="{'sidebar--closed': isNavClosed}"
     >
-      <button 
+      <button
         class="w3-xxlarge w3-padding w3-button w3-hover-green"
         v-bind:class="{'w3-display-topright': isNavOpened}"
-        @click="toggleNav"  
+        @click="toggleNav"
       >
         <i v-if="isNavClosed" class="fa fa-bars"></i>
         <i v-if="isNavOpened" class="fa fa-times"></i>
@@ -20,58 +20,72 @@
       </div>
 
       <div style="margin-top: 20vh">
-        <inertia-link 
-          href="/home" 
+        <inertia-link
+          href="/home"
           class="w3-bar-item w3-button w3-hover-gray"
           :class="{'w3-light-gray': path === '/home' || path === '/'}"
         >
-          <i class="fa fa-home w3-xxlarge"></i> 
+          <i class="fa fa-home w3-xxlarge"></i>
           <span v-if="isNavOpened" class="w3-xlarge">Acasa</span>
         </inertia-link>
 
-        <inertia-link 
-          href="/users" 
+        <inertia-link
+          href="/users"
           class="w3-bar-item w3-button w3-hover-gray"
           :class="{'w3-light-gray': path === '/users'}"
         >
-          <i class="fa fa-users w3-xxlarge"></i> 
+          <i class="fa fa-users w3-xxlarge"></i>
           <span v-if="isNavOpened" class="w3-xlarge">Amministratori</span>
         </inertia-link>
 
-        <inertia-link 
-          href="/standard-requests" 
+        <inertia-link
+          href="/standard-requests"
           class="w3-bar-item w3-button w3-hover-gray"
           :class="{'w3-light-gray': path === '/standard-requests'}"
         >
-          <i class="fa fa-upload w3-xxlarge"></i> 
+          <i class="fa fa-upload w3-xxlarge"></i>
           <span v-if="isNavOpened" class="w3-xlarge">Richeste</span>
         </inertia-link>
 
-        <inertia-link 
-          href="/express-requests" 
+        <inertia-link
+          href="/express-requests"
           class="w3-bar-item w3-button w3-hover-gray"
           :class="{'w3-light-gray': path === '/express-requests'}"
         >
-          <i class="fa fa-truck w3-xxlarge"></i> 
+          <i class="fa fa-truck w3-xxlarge"></i>
           <span v-if="isNavOpened" class="w3-xlarge">Richeste Express</span>
         </inertia-link>
 
-        <inertia-link 
-          href="/push-notifications/create" 
+        <inertia-link
+          href="/push-notifications/create"
           class="w3-bar-item w3-button w3-hover-gray"
           :class="{'w3-light-gray': path === '/push-notifications/create'}"
         >
-          <i class="fa fa-bell w3-xxlarge"></i> 
+          <i class="fa fa-bell w3-xxlarge"></i>
           <span v-if="isNavOpened" class="w3-xlarge">Notifiche Push</span>
         </inertia-link>
       </div>
     </nav>
     <div :style="{marginLeft: isNavClosed ? '70px' : '20%'}">
       <header>
-        <h1 class="w3-green w3-padding"> 
+        <h1 class="w3-green w3-padding">
           <span style="font-family: Times New Roman">Lemart &amp; Lemart</span> Express Control Panel
         </h1>
       </header>
+
+        <div v-for="(value, name) in $page.flash" :key="name">
+            <div
+                v-if="value && displayFlashMessages.indexOf(name) !== -1"
+                class="w3-card-4 flash-message w3-border-blue w3-leftbar w3-padding w3-display-container"
+            >
+                {{value}}
+                <span
+                    class="w3-button w3-display-right"
+                    @click="displayFlashMessages = displayFlashMessages.filter(msgName => msgName !== name)"
+                >&times;</span>
+            </div>
+        </div>
+
       <div class="w3-container">
         <slot />
       </div>
@@ -80,6 +94,12 @@
 </template>
 
 <style>
+
+    .flash-message {
+        width: 90%;
+        margin-left: 3%;
+    }
+
   body {
     background-color: lightgray;
     margin: 8px;
@@ -104,17 +124,20 @@ export default {
   data: function() {
     return {
       isNavClosed: true,
+      displayFlashMessages: Object.keys(this.$page.flash),
     };
   },
 
   methods: {
-    toggleNav: function() { this.isNavClosed = !this.isNavClosed }
+    toggleNav: function() { this.isNavClosed = !this.isNavClosed },
   },
 
   computed: {
     isNavOpened: function() { return !this.isNavClosed; },
     path: function() { return window.location.pathname; },
-  }
+  },
+
+  mounted: function() { console.log(this.$page.flash)}
 }
 </script>
 
