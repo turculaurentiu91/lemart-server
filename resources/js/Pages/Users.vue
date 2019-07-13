4a6927899753d896004b474f91a637feed472c21
Laurentiu Turcu <turculaurentiu91@gmail.com>
2019-07-13 08:03:50 +0200

    made the users tabe responsive too

--- resources/js/Pages/Users.vue
+++ resources/js/Pages/Users.vue
@@ -14,46 +14,47 @@
 
       <inertia-link href="/users/create" class="w3-button w3-green w3-margin-left"><i class="fa fa-plus"></i> Aggiungi Nuovo</inertia-link>
     </div>
-
-    <table class="w3-table-all w3-hoverable w3-margin-top">
-      <thead>
-        <tr>
-          <th>ID</th>
-          <th>Nome</th>
-          <th>Email</th>
-          <th>Creato il</th>
-          <th>Modificato il</th>
-          <th>Modifica</th>
-          <th>Cancella</th>
-        </tr>
-      </thead>
-      <tbody>
-        <tr v-for="user in $page.users.data" v-bind:key="user.id">
-          <td>{{user.id}}</td>
-          <td>{{user.name}}</td>
-          <td>{{user.email}}</td>
-          <td>{{user.created_at}}</td>
-          <td>{{user.updated_at}}</td>
-          <td>
-            <inertia-link
-              :href="`/users/edit/${user.id}`"
-              class="w3-button w3-text-blue"
-            >
-              <i class="fa fa-edit w3-xlarge"></i>
-            </inertia-link>
-          </td>
-          <td>
-            <button
-              class="w3-button w3-text-red"
-              method="delete"
-              @click.prevent="() => setupUserForDeletion(user)"
-            >
-              <i class="fa fa-times w3-xlarge"></i>
-            </button>
-          </td>
-        </tr>
-      </tbody>
-    </table>
+    <div class="w3-responsive">
+        <table class="w3-table-all w3-hoverable w3-margin-top">
+            <thead>
+                <tr>
+                <th>ID</th>
+                <th>Nome</th>
+                <th>Email</th>
+                <th>Creato il</th>
+                <th>Modificato il</th>
+                <th>Modifica</th>
+                <th>Cancella</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr v-for="user in $page.users.data" v-bind:key="user.id">
+                <td>{{user.id}}</td>
+                <td>{{user.name}}</td>
+                <td>{{user.email}}</td>
+                <td>{{user.created_at}}</td>
+                <td>{{user.updated_at}}</td>
+                <td>
+                    <inertia-link
+                    :href="`/users/edit/${user.id}`"
+                    class="w3-button w3-text-blue"
+                    >
+                    <i class="fa fa-edit w3-xlarge"></i>
+                    </inertia-link>
+                </td>
+                <td>
+                    <button
+                    class="w3-button w3-text-red"
+                    method="delete"
+                    @click.prevent="() => setupUserForDeletion(user)"
+                    >
+                    <i class="fa fa-times w3-xlarge"></i>
+                    </button>
+                </td>
+                </tr>
+            </tbody>
+        </table>
+    </div>
 
     <div class="w3-bar w3-margin">
       <inertia-link
